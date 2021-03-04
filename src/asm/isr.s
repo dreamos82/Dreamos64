108a0f4520cb5f5894dc6bdf1a088045992bb608
Ivan Gualandri <dreamos82@yahoo.it>
2021-03-04 16:19:40 +0000

    Fix indentation

--- src/asm/isr.s
+++ src/asm/isr.s
@@ -5,16 +5,16 @@
 %macro interrupt_service_routine 1
 [global interrupt_service_routine_%1]
 interrupt_service_routine_%1:
-	cli
-	push rax
-	push rcx
-	push rdx
-	push rbx
-	push rsp
-	push rbp
-	push rsi
-	push rdi
-	push r8
+    cli
+    push rax
+    push rcx
+    push rdx
+    push rbx
+    push rsp
+    push rbp
+    push rsi
+    push rdi
+    push r8
     push r9
     push r10
     push r11
@@ -23,26 +23,26 @@ interrupt_service_routine_%1:
     push r14
     push r15
     mov rdi, %1
-	cld
-	call interrupts_handler
-	pop r15
-	pop r14
-	pop r13
-	pop r12
-	pop r11
-	pop r10
-	pop r9
-	pop r8
-	pop rdi
-	pop rsi
-	pop rsi
-	pop rbp
-	pop rsp
-	pop rbx
-	pop rdx
-	pop rcx
-	pop rax	
-	iretq
+    cld
+    call interrupts_handler
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rsi
+    pop rbp
+    pop rsp
+    pop rbx
+    pop rdx
+    pop rcx
+    pop rax
+    iretq
 %endmacro
 
 interrupt_service_routine 14
