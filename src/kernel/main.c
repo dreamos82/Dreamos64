b12745fb62dcd3cfa91eee1c53d2a8a5de13e708
Ivan G <59960116+dreamos82@users.noreply.github.com>
2021-06-07 20:01:26 +0100

    Read multiboot information in the bootloader (#37)
    Read multiboot data from bootloader

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -19,60 +19,64 @@
 #include <pmm.h>
 #include <mmap.h>
 
-struct multiboot_tag_framebuffer *tagfb = NULL;
-struct multiboot_tag_basic_meminfo *tagmem = NULL;
-struct multiboot_tag_old_acpi *tagold_acpi = NULL;
-struct multiboot_tag_mmap *tagmmap = NULL;
 extern char _binary_fonts_default_psf_size;
 extern char _binary_fonts_default_psf_start;
 extern char _binary_fonts_default_psf_end;
 extern uint32_t FRAMEBUFFER_MEMORY_SIZE;
+extern uint64_t multiboot_framebuffer_data;
+extern uint64_t multiboot_mmap_data;
+extern uint64_t multiboot_basic_meminfo;
+extern uint64_t multiboot_acpi_info;
+struct multiboot_tag_framebuffer *tagfb = NULL;
+struct multiboot_tag_basic_meminfo *tagmem = NULL;
+struct multiboot_tag_old_acpi *tagold_acpi = NULL;
+struct multiboot_tag_mmap *tagmmap = NULL;
+struct multiboot_tag *tagacpi = NULL;
 
 void _read_configuration_from_multiboot(unsigned long addr){
     struct multiboot_tag* tag;
+    tagmem  = (struct multiboot_tag_basic_meminfo *)(multiboot_basic_meminfo + _HIGHER_HALF_KERNEL_MEM_START);
+    tagmmap = (struct multiboot_tag_mmap *) (multiboot_mmap_data + _HIGHER_HALF_KERNEL_MEM_START);
+    tagfb   = (struct multiboot_tag_framebuffer *) (multiboot_framebuffer_data + _HIGHER_HALF_KERNEL_MEM_START);
+    //Print basic mem Info data
+    _printStringAndNumber("Found basic mem Mem info type: ", tagmem->type);
+    _printStringAndNumber("Memory lower (in kb): ", tagmem->mem_lower);
+    _printStringAndNumber("Memory upper (in kb): ", tagmem->mem_upper); 
+    //Print framebuffer info
+    _printStringAndNumber("Found multiboot framebuffer: ", tagmem->type); 
+    _printStringAndNumber("---framebuffer-type: ", tagfb->common.framebuffer_type);
+    _printStringAndNumber("---framebuffer-width: ", tagfb->common.framebuffer_width);
+    _printStringAndNumber("---framebuffer-height: ", tagfb->common.framebuffer_height);
+    _printStringAndNumber("---framebuffer-address: ", tagfb->common.framebuffer_addr);
+    _printStringAndNumber("---framebuffer-bpp: ", tagfb->common.framebuffer_bpp);
+    _printStringAndNumber("---framebuffer-pitch: ", tagfb->common.framebuffer_pitch);
+    set_fb_data(tagfb);
+    _printStringAndNumber("---Total framebuffer size is:  ", FRAMEBUFFER_MEMORY_SIZE);
+    //Print mmap_info
+    _printStringAndNumber("Memory map entry: ", tagmmap->type);
+    _printStringAndNumber("---Size: ", tagmmap->size);
+    _printStringAndNumber("---Entrysize: ", tagmmap->entry_size);
+    _printStringAndNumber("---EntryVersion: ", tagmmap->entry_version);
+    _printStringAndNumber("---Struct size: ", sizeof(struct multiboot_tag_mmap));
+    _mmap_parse(tagmmap);
+
+    tagacpi = (struct multiboot_tag *) (multiboot_acpi_info + _HIGHER_HALF_KERNEL_MEM_START);
+    if(tagacpi->type == MULTIBOOT_TAG_TYPE_ACPI_OLD){
+        tagold_acpi = (struct multiboot_tag_old_acpi *)tagacpi;
+        _printStringAndNumber("Found acpi RSDP: ", tagold_acpi->type);
+        _printStringAndNumber("Found acpi RSDP address: ", (unsigned long) &tagold_acpi);
+        RSDPDescriptor *descriptor = (RSDPDescriptor *)(tagacpi+1);
+        _printStringAndNumber("Address: ", (unsigned long) &descriptor);
+        _printStringAndNumber("Descriptor revision: ", descriptor->Revision);
+        validate_RSDP(descriptor);
+    }
+ 
 	for (tag=(struct multiboot_tag *) (addr + _HIGHER_HALF_KERNEL_MEM_START + 8);
 		tag->type != MULTIBOOT_TAG_TYPE_END;
 		tag = (struct multiboot_tag *) ((multiboot_uint8_t *) tag 
 										+ ((tag->size + 7) & ~7))){
         switch(tag->type){
-            case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
-                tagmem = (struct multiboot_tag_basic_meminfo *) tag;
-                _printStringAndNumber("Found basic mem Mem info type: ", tagmem->type);
-                _printStringAndNumber("Memory lower (in kb): ", tagmem->mem_lower);
-                _printStringAndNumber("Memory upper (in kb): ", tagmem->mem_upper); 
-               break;
-            case MULTIBOOT_TAG_TYPE_ACPI_OLD:
-                tagold_acpi = (struct multiboot_tag_old_acpi *)tag;
-                _printStringAndNumber("Found acpi RSDP: ", tagold_acpi->type);
-                _printStringAndNumber("Found acpi RSDP address: ", (unsigned long) &tagold_acpi);
-                RSDPDescriptor *descriptor = (RSDPDescriptor *)(tag+1);
-                _printStringAndNumber("Address: ", (unsigned long) &descriptor);
-                _printStringAndNumber("Descriptor revision: ", descriptor->Revision);
-                validate_RSDP(descriptor);
-                //parse_RSDT(descriptor);
-                break;
-            case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
-                _printStringAndNumber("Found multiboot framebuffer: ", tag->type); 
-                tagfb = (struct multiboot_tag_framebuffer *) tag;
-                _printStringAndNumber("---framebuffer-type: ", tagfb->common.framebuffer_type);
-                _printStringAndNumber("---framebuffer-width: ", tagfb->common.framebuffer_width);
-                _printStringAndNumber("---framebuffer-height: ", tagfb->common.framebuffer_height);
-                _printStringAndNumber("---framebuffer-address: ", tagfb->common.framebuffer_addr);
-                _printStringAndNumber("---framebuffer-bpp: ", tagfb->common.framebuffer_bpp);
-                _printStringAndNumber("---framebuffer-pitch: ", tagfb->common.framebuffer_pitch);
-                set_fb_data(tagfb);
-                _printStringAndNumber("---Total framebuffer size is:  ", FRAMEBUFFER_MEMORY_SIZE);
-                break;
-            case MULTIBOOT_TAG_TYPE_MMAP:
-                tagmmap = (struct multiboot_tag_mmap*) tag;
-                _printStringAndNumber("Memory map entry: ", tagmmap->type);
-                _printStringAndNumber("---Size: ", tagmmap->size);
-                _printStringAndNumber("---Entrysize: ", tagmmap->entry_size);
-                _printStringAndNumber("---EntryVersion: ", tagmmap->entry_version);
-                _printStringAndNumber("---Struct size: ", sizeof(struct multiboot_tag_mmap));
-                _mmap_parse(tagmmap);
-                break;
-            case MULTIBOOT_TAG_TYPE_ELF_SECTIONS:
+           case MULTIBOOT_TAG_TYPE_ELF_SECTIONS:
                 _printStr("Found elf sections");
                 _printNewLine();
                 break;
@@ -82,8 +86,8 @@ void _read_configuration_from_multiboot(unsigned long addr){
                 break;
         }
     }
+    _printStringAndNumber("Size of end tag: ", tag->size);
     _printStr("End of read configuration\n");
-
 }
 void kernel_start(unsigned long addr, unsigned long magic){
     //struct multiboot_tag *tag;
@@ -137,6 +141,9 @@ void kernel_start(unsigned long addr, unsigned long magic){
             qemu_write_string("PSF v2 found\n");
         }
         _printStringAndNumber("Size of psv1_font: ", sizeof(PSFv1_Font));
+        for(int i = 3; i<23; i++){
+            _fb_printStr("Test Line", 1, i, 0x000000, 0xFFFFFF);
+        }
     #endif
     char *cpuid_model = _cpuid_model();
     _printStr("Cpuid model: ");
@@ -148,7 +155,7 @@ void kernel_start(unsigned long addr, unsigned long magic){
     init_apic();
     pmm_setup();
     pmm_reserve_area(0x10000, 10);
-    //_printStringAndNumber("faulter: ", *((uint64_t *)0xffffffff71012342));
+    _printStringAndNumber("--ACPI_INFO: ", multiboot_acpi_info);
     asm("hlt");
 }
 
