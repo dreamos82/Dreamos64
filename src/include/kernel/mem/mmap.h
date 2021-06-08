b3cc44bade1a573f48bb1fde4c8bcfefa0413d0d
Ivan Gualandri <dreamos82@yahoo.it>
2021-06-08 23:36:06 +0100

    Remove mmap_wrapper structure

--- src/include/kernel/mem/mmap.h
+++ src/include/kernel/mem/mmap.h
@@ -4,11 +4,6 @@
 #include <multiboot.h>
 #include <stdint.h>
 
-typedef struct {
-    uint32_t number_of_entries;
-    multiboot_memory_map_t *entries;
-} mmap_wrapper;
-
 extern const char *mmap_types[];
 
 void _mmap_parse(struct multiboot_tag_mmap*);
--- src/kernel/mem/mmap.c
+++ src/kernel/mem/mmap.c
@@ -10,6 +10,8 @@
 extern uint32_t used_frames;
 extern struct multiboot_tag_basic_meminfo *tagmem;
 mmap_wrapper mmap_data;
+uint32_t mmap_number_of_entries;
+multiboot_memory_map_t *mmap_entries;
 
 const char *mmap_types[] = {
     "Invalid",
@@ -22,18 +24,18 @@ const char *mmap_types[] = {
 
 void _mmap_parse(struct multiboot_tag_mmap *mmap_root){
     int total_entries = 0;
-    mmap_data.number_of_entries = (mmap_root->size - sizeof(*mmap_root))/mmap_root->entry_size;
-    mmap_data.entries = mmap_root->entries;
+    mmap_number_of_entries = (mmap_root->size - sizeof(*mmap_root))/mmap_root->entry_size;
+    mmap_entries = mmap_root->entries;
 #ifndef _TEST_
     uint32_t i=0;
-    while(i<mmap_data.number_of_entries){
-        _printStringAndNumber("Address: ", (uint32_t)mmap_data.entries[i].addr);
-        _printStringAndNumber("---Len: ", (uint32_t)mmap_data.entries[i].len);
-        _printStringAndNumber("---Type:: ", mmap_data.entries[i].type);
+    while(i<mmap_number_of_entries){
+        _printStringAndNumber("Address: ", (uint32_t)mmap_entries[i].addr);
+        _printStringAndNumber("---Len: ", (uint32_t)mmap_entries[i].len);
+        _printStringAndNumber("---Type:: ", mmap_entries[i].type);
         _printStr("Type str: ");
-        _printStr((char *) mmap_types[mmap_data.entries[i].type]);
+        _printStr((char *) mmap_types[mmap_entries[i].type]);
         _printNewLine();
-        _printStringAndNumber("---zero:: ", mmap_data.entries[i].zero);
+        _printStringAndNumber("---zero:: ", mmap_entries[i].zero);
         total_entries++;
         i++;
     }
@@ -46,11 +48,11 @@ void _mmap_setup(){
     if(used_frames > 0){
         uint32_t counter = 0;
         uint64_t mem_limit = (tagmem->mem_upper + 1024) * 1024;
-        while(counter < mmap_data.number_of_entries){
-            if(mmap_data.entries[counter].addr < mem_limit &&
-                    mmap_data.entries[counter].type > 1){
-               _printStringAndNumber("Found entry at addr: ", mmap_data.entries[counter].addr);
-               pmm_reserve_area(mmap_data.entries[counter].addr, mmap_data.entries[counter].len);
+        while(counter < mmap_number_of_entries){
+            if(mmap_entries[counter].addr < mem_limit &&
+                    mmap_entries[counter].type > 1){
+               _printStringAndNumber("Found entry at addr: ", mmap_entries[counter].addr);
+               pmm_reserve_area(mmap_data.entries[counter].addr, mmap_entries[counter].len);
             }
             counter++;
         }
