bc3d0b1b84027be3f851bb48dcaa509d106cb224
Ivan Gualandri <dreamos82@yahoo.it>
2021-09-19 16:37:22 +0100

    Working on mapping function

--- src/kernel/mem/vmm.c
+++ src/kernel/mem/vmm.c
@@ -32,20 +32,25 @@ void *map_vaddress(void *address, size_t size, int flags){
 	if(pml4_table[pml4_e] == 0){
 		_printStr("PML4 Entry is 0, need to create a new one");
 		uint64_t *new_table = pmm_alloc_frame();
+		pml4_table[pml4_e] = (uint64_t) new_table | WRITE_BIT | PRESENT_BIT;
 		clean_new_table(new_table);
 	}
-	_printStringAndNumber("P4_table[pt_e]: ", p4_table[pml4_e]);
+	_printStringAndNumber("P4_table[pml4_e]: ", p4_table[pml4_e]);
     _printStringAndNumber("PML4 Value: ", pml4_table[pml4_e]);
     _printStringAndNumber("P4_table[511]: ", pml4_table[511]);
     _printStringAndNumber("PML4 Value: ", pml4_table[511]);
     uint16_t pdpr_e = PDPR_ENTRY((uint64_t) address);
 	uint64_t *pdpr_table = SIGN_EXTENSION | ENTRIES_TO_ADDRESS(510l,510l,510l,pml4_e);
+	_printStr(" -----");
     _printStringAndNumber("PDPR Value: ", pdpr_e);
-    _printStringAndNumber("PML4 Value: ", pdpr_table[0]);
-    _printStringAndNumber("P4_table[511]: ", pdpr_table[pdpr_e]);
-    _printStringAndNumber("PML4 Value: ", pdpr_table[pdpr_e]);
+    _printStringAndNumber("PDPR Table Value: ", pdpr_table[0]);
+    _printStringAndNumber("PDPR Table[pdpr_e]: ", pdpr_table[pdpr_e]);
+	_printStr(" -----");
 	if(pdpr_table[pdpr_e] == 0){
 		_printStr("PDPR - Entry is 0, need to create a new one");
+		uint64_t *new_table = pmm_alloc_frame();
+		pdpr_table[pdpr_e] = (uint64_t) new_table | WRITE_BIT | PRESENT_BIT;
+		clean_new_table(new_table);
 	}
 
     uint16_t pd_e = PD_ENTRY((uint64_t) address);
