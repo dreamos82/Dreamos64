29ade689c32714fce4230dc0a053a0c0642d5248
Ivan Gualandri <dreamos82@yahoo.it>
2021-09-21 19:53:29 +0100

    Finish mapping function draft, it works apparently

--- src/kernel/mem/vmm.c
+++ src/kernel/mem/vmm.c
@@ -58,7 +58,7 @@ void *map_vaddress(void *address, size_t size, int flags){
 		#if SMALL_PAGES == 1
 		pd_table[pd_e] = (uint64_t) new_table | WRITE_BIT | PRESENT_BIT;
 		#elif SMALL_PAGES == 0
-		pd_table[pd_e] = (uint64_t) new_table | WRITE_BIT | PRESENT_BIT;
+		pd_table[pd_e] = (uint64_t) new_table | WRITE_BIT | PRESENT_BIT | HUGEPAGE_BIT;
 		clean_new_table(new_table);
 		#endif
 	}
@@ -68,15 +68,19 @@ void *map_vaddress(void *address, size_t size, int flags){
     //_printStringAndNumber("Computed_address: ", computed_address[0]);
     #if SMALL_PAGES == 1
 	uint64_t *pt_table = SIGN_EXTENSION | ENTRIES_TO_ADDRESS(510l, pml4_e, pdpr_e, pd_e);
-    uint16_t pt_e = PT_Entry((uint64_t) address);
+    uint16_t pt_e = PT_ENTRY((uint64_t) address);
     _printStringAndNumber("PT_Entry value: ", pt_e);
 	if(pt_table[pt_e] == 0) {
 		_printStr("PT_Entry is 0, need just to set it present!!");
+		uint64_t *new_table = pmm_alloc_frame();
+		pt_table[pt_e] = (uint64_t) new_table | WRITE_BIT | PRESENT_BIT;
 	} 
 
     #elif SMALL_PAGE == 0 
-    //Later
+    _printStr("No need to check for page table\n");
     #endif
+
+	return address;
 }
 
 
