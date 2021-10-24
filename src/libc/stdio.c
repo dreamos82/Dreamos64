297277ba2766da761dff54818ad3b6b3b0c88953
Dean <dean243@hotmail.com>
2021-10-25 00:23:11 +1100

    Fixed typo and minor bug.

--- src/libc/stdio.c
+++ src/libc/stdio.c
@@ -22,11 +22,12 @@ int vsprintf(char *buffer, const char *fmt, va_list args){
         }
         fmt++;
         switch(*fmt){
-            case 'd':
+            case 'd':{
                 int number = va_arg(args, int);
                 int string_size = _getDecString(str, number);
                 str+= string_size;
                 break;
+            }
             case 'x':{
                 int number = va_arg(args, int);
                 int string_size = _getHexString(str, number);
