5d1468909f1b21faa3ec670c9112b968a90ab3ee
Ivan G <59960116+dreamos82@users.noreply.github.com>
2021-11-11 11:07:43 +0000

    RDMsr function (#57)
    Squad of PR#57: Added rdmsr()
    
    * Add draft of rdmsr C function
    
    * Return proper value in rdmsr

---
+++ src/kernel/arch/x86_64/cpu/msr.c
@@ -0,0 +1,15 @@
+#include <msr.h>
+#include <stdio.h>
+#include <stddef.h>
+
+uint64_t rdmsr(uint32_t address){
+    uint32_t low=0, high=0;
+    printf("TBD\n");
+    asm("movl %2, %%ecx;" 
+        "rdmsr;"
+        : "=a" (low), "=d" (high)
+        : "g" (address)
+    );
+
+    return (uint64_t) low | (high << 32);
+}
