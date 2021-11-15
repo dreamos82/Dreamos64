42a5c7c01c7672ac36a5f248b1997840adf3374e
Ivan Gualandri <dreamos82@yahoo.it>
2021-11-15 01:14:24 +0000

    Fix serial logging for VBOX

--- src/kernel/io/serial.c
+++ src/kernel/io/serial.c
@@ -5,12 +5,12 @@ int init_serial(int port){
 
    outportb(port + 1, 0x00);    // Disable all interrupts
    outportb(port + 3, 0x80);    // Enable DLAB (set baud rate divisor)
-   outportb(port + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
+   outportb(port + 0, 0x0C);    // Set divisor to 3 (lo byte) 38400 baud
    outportb(port + 1, 0x00);    //                  (hi byte)
    outportb(port + 3, 0x03);    // 8 bits, no parity, one stop bit
    outportb(port + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
    outportb(port + 4, 0x0B);    // IRQs enabled, RTS/DSR set
-   outportb(port + 4, 0x1E);    // Set in loopback mode, test the serial chip
+//   outportb(port + 4, 0x1E);    // Set in loopback mode, test the serial chip
    outportb(port + 0, 0xAE);    // Test serial chip (send byte 0xAE and check if serial returns same byte)
 
    // Check if serial is faulty (i.e: not same byte as sent)
