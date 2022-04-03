cce2b07c928467dd20fc5e5b78acf8e0e39e1719
Ivan G <59960116+dreamos82@users.noreply.github.com>
2022-04-03 07:56:42 +0100

    100 implement keyb config read (#102)
    * Read keyboard_config and add kernel_status struct
    
    * Add few define for ps2/keyboard
    
    * Fix typos

--- src/include/kernel/hardware/keyboard.h
+++ src/include/kernel/hardware/keyboard.h
@@ -2,7 +2,10 @@
 #define __KEYBOARD_H_
 
 #define KEYBOARD_ENCODER_PORT   0x60
+#define KEYBOARD_STATUS_REGISTER   0x64
+#define KEYBOARD_COMMAND_REGISTER   KEYBOARD_SATATUS_REGISTER
 #define KEY_RELEASE_MASK    0x80
+#define KEYBOARD_ACK_BYTE   0xFA
 
 void init_keyboard();
 void handle_keyboard_interrupt();
---
+++ src/include/kernel/kernel.h
@@ -0,0 +1,18 @@
+#ifndef _KERNEL_H_
+#define _KERNEL_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+struct keyboard_status_t {
+    uint8_t scancode_set;
+    bool    translation_enabled;
+};
+
+typedef struct kernel_status_t {
+
+    struct keyboard_status_t keyboard;
+} kernel_status_t;
+
+
+#endif
