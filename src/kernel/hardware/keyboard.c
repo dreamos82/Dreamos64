10c0a974bc1a7715235958eac363b1a5a15fc14e
Ivan G <59960116+dreamos82@users.noreply.github.com>
2022-04-09 15:21:38 +0100

    103 keycodes buffer (#104)
    * Add draft for keypress buffer
    
    * Trying to define some translation mechanism
    
    * Add keyboard mapping

--- src/kernel/hardware/keyboard.c
+++ src/kernel/hardware/keyboard.c
@@ -5,7 +5,21 @@
 #include <ps2.h>
 #include <kernel.h>
 
-extern kernel_status_t kernel_settings;
+//extern kernel_status_t kernel_settings;
+
+key_status keyboard_buffer[MAX_KEYB_BUFFER_SIZE];
+key_codes scancode_mappings[] = {
+0, ESCAPE, D1, D2, D3, D4, D5, D6, D7, D8, D9, D0, MINUS, EQUALS, BACKSPACE,
+TAB, Q, W, E, R, T, Y, U, I, O, P, SQBRACKET_OPEN, SQBRACKET_CLOSE, ENTER,
+LEFT_CTRL, A, S, D, F, G, H, J, K, L, SEMICOLON, SINGLE_QUOTE, BACK_TICK,
+LEFT_SHIFT, SLASH, Z, X, C, V, B, N, M, COMMA, DOT, BACKSLASH, RIGHT_SHIFT,
+KEYPAD_STAR, LEFT_ALT, SPACE, CAPS_LOCK,
+F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, 
+F11, F12, NUM_LOCK, SCROLL_LOCK,
+KEYPAD_D7, KEYPAD_D8, KEYPAD_D9, KEYPAD_MINUS, KEYPAD_D4, KEYPAD_D5, KEYPAD_D6, KEYPAD_PLUS, KEYPAD_D1, KEYPAD_D2, KEYPAD_D3, KEYPAD_D0, KEYPAD_DOT,
+};
+
+size_t buf_position;
 
 void init_keyboard(){
     //Let's do a keyboard read just to make sure it's empty
@@ -32,26 +46,41 @@ void init_keyboard(){
         printf("Translation enabled\n");
         kernel_settings.keyboard.translation_enabled = true;
     } else {
-        kernel_settings.keyboard.translation_enabled = false;
+        kernel_settings.keyboard.translation_enabled = false;        
     }
+    buf_position = 0;
 }
 
 void handle_keyboard_interrupt() {
     
-    int scancode = inportb(KEYBOARD_ENCODER_PORT);    
-    
-    if(scancode & KEY_RELEASE_MASK) {
-        #if USE_FRAMEBUFFER == 1
-            _fb_printStrAndNumber(" Key released: 0x", scancode, 0, 10, 0x000000, 0xE169CD);
-
-        #endif
-
-        printf("---A key is released booh... %x\n", scancode);
-    } else {
-        #if USE_FRAMEBUFFER == 1
-            _fb_printStrAndNumber("  Key pressed: 0x", scancode, 0, 10, 0x000000, 0xE169CD);
-
-        #endif
-        printf("---A key is pressed hooray... %x\n", scancode);
+    int scancode = inportb(KEYBOARD_ENCODER_PORT);
+  
+    if(kernel_settings.keyboard.translation_enabled == true || kernel_settings.keyboard.scancode_set) {
+        keyboard_buffer[buf_position].code = translate(scancode);
+        if(scancode & KEY_RELEASE_MASK) {
+            SET_RELEASED_STATUS(keyboard_buffer[buf_position].modifiers);
+            #if USE_FRAMEBUFFER == 1
+                _fb_printStrAndNumber(" Key released: 0x", translate(scancode), 0, 10, 0x000000, 0xE169CD);
+            #endif
+            printf("---A key is released: %d - SC:  %x - Code:  %x - Mod: 0x%x\n", buf_position, scancode, keyboard_buffer[buf_position].code, keyboard_buffer[buf_position].modifiers);
+         } else {
+            SET_PRESSED_STATUS(keyboard_buffer[buf_position].modifiers);
+            #if USE_FRAMEBUFFER == 1
+                _fb_printStrAndNumber("  Key pressed: 0x", translate(scancode), 0, 10, 0x000000, 0xE169CD);
+            #endif
+            printf("---A key is pressed %d -  SC: %x - Code: %x - Mod: 0x%x\n", buf_position, scancode, keyboard_buffer[buf_position].code, keyboard_buffer[buf_position].modifiers);
+        }
+        buf_position = BUF_STEP(buf_position);
     } 
+}
+
+key_codes translate(uint8_t scancode) {
+    if(scancode == 0xE0) {
+        printf("Extended byte\n");
+    }
+    if(scancode < 0x47) {
+        printf("Translated: %x\n", scancode_mappings[scancode]);
+        return scancode_mappings[scancode];
+    }
+    return scancode_mappings[0];
 }
