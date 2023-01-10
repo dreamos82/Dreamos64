d8f0737bda1e3b31f8135bad4965013aa4c6a946
Ivan Gualandri <dreamos82@yahoo.it>
2023-01-10 10:51:06 +0100

    working on vfs close

--- src/drivers/fs/ustar.c
+++ src/drivers/fs/ustar.c
@@ -5,3 +5,7 @@ int ustar_open(char *path, int flags) {
     loglinef(Verbose, "ustar_open_called with path: %s and flags: %d", path, flags);
     return 3;
 }
+
+int ustar_close(int ustar_fildes) {
+
+}
--- src/libc/unistd.c
+++ src/libc/unistd.c
@@ -5,9 +5,18 @@
 int close (int fildes) {
     if (fildes < OPENEDFILES_MAX) {
         //TODO: it should check for opened files by all threads.
+        if (vfs_opened_files[fildes].fs_specific_id > 0) {
+            int fs_specific_id = vfs_opened_files[fildes].fs_specific_id;
+            int mountpoint_id = vfs_opened_files[fildes].mountpoint_id;
+            mountpoint_t mountpoint = mountpoints[mountpoint_id];
+            if (mountpoint.file_operations.close != NULL) {
+                mountpoint.file_operations.close(fs_specific_id);
+            }
+        }
         vfs_opened_files[fildes].fs_specific_id = -1;
         vfs_opened_files[fildes].mountpoint_id = -1;
         vfs_opened_files[fildes].buffer_read_pos = -1;
+        return 0;
     }
     return -1;
 }
