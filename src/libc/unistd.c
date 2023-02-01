ab99566cf170a9c9ff7986c5b4f8f215fd6d67c8
Ivan Gualandri <dreamos82@yahoo.it>
2023-02-01 16:40:48 +0100

    minor changes

--- src/libc/unistd.c
+++ src/libc/unistd.c
@@ -25,14 +25,14 @@ int close (int fildes) {
 ssize_t read(int fildes, void *buf, size_t nbytes){
     //Will be implemented in the future
     if (fildes >= 0 && fildes < OPENEDFILES_MAX) {
-        if (vfs_opened_files[fildes].fs_specific_id > 0) {
+        if (vfs_opened_files[fildes].fs_specific_id >= 0) {
             int fs_specific_id = vfs_opened_files[fildes].fs_specific_id;
             int mountpoint_id = vfs_opened_files[fildes].mountpoint_id;
             mountpoint_t mountpoint = mountpoints[mountpoint_id];
             if (mountpoint.file_operations.read != NULL) {
-                mountpoint.file_operations.read(fs_specific_id, buf, nbytes);
+                return mountpoint.file_operations.read(fs_specific_id, buf, nbytes);
             }
-            return nbytes;
+            return 0;
         }
     }
     return -1;
