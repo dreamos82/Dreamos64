ae179810367795377ba442c1cd0fb2ef39b7c818
Ivan Gualandri <dreamos82@yahoo.it>
2023-05-23 23:15:50 +0100

    clean make output of ioapic more readable

--- src/kernel/arch/x86_64/cpu/ioapic.c
+++ src/kernel/arch/x86_64/cpu/ioapic.c
@@ -16,15 +16,15 @@ void init_ioapic(MADT *madt_table){
     MADT_Item* item = get_MADT_item(madt_table, MADT_IO_APIC, 0);
     io_apic_source_override_array_size = 0;
     if(item != NULL) {
-        loglinef(Verbose, "IOAPIC Item type: 0x%x", item->type);
-        loglinef(Verbose, "IOAPIC Item address: 0x%x - length: 0x%x", item, item->length);
+        loglinef(Verbose, "(init_ioapic):  Item type: 0x%x", item->type);
+        loglinef(Verbose, "(init_ioapic): IOAPIC Item address: 0x%x - length: 0x%x", item, item->length);
         IO_APIC_Item *ioapic_item = (IO_APIC_Item *) ( ensure_address_in_higher_half((uint64_t) item + sizeof(MADT_Item)));
         if (is_phyisical_address_mapped(ALIGN_PHYSADDRESS((uint64_t) item), ensure_address_in_higher_half((uint64_t) item)) == PHYS_ADDRESS_NOT_MAPPED) {
             map_phys_to_virt_addr((void *) ALIGN_PHYSADDRESS((uint64_t) item), (void *)ensure_address_in_higher_half((uint64_t) item),0);
         }
-        loglinef(Verbose, "IOAPIC_ID: 0x%x, Address: 0x%x", ioapic_item->ioapic_id, ioapic_item->address ); 
-        loglinef(Verbose, "IOApic_Global_System_Interrupt_Base: 0x%x", ioapic_item->global_system_interrupt_base);
-        loglinef(Verbose, "IOApic Higher Half Address: 0x%x", ensure_address_in_higher_half(ioapic_item->address));
+        loglinef(Verbose, "(init_ioapic): IOAPIC_ID: 0x%x, Address: 0x%x", ioapic_item->ioapic_id, ioapic_item->address ); 
+        loglinef(Verbose, "(init_ioapic): IOApic_Global_System_Interrupt_Base: 0x%x", ioapic_item->global_system_interrupt_base);
+        loglinef(Verbose, "(init_ioapic): IOApic Higher Half Address: 0x%x", ensure_address_in_higher_half(ioapic_item->address));
         io_apic_base_address = ioapic_item->address;
         io_apic_hh_base_address = ensure_address_in_higher_half(ioapic_item->address);
         // This one should be mapped in the higher half ?? 
@@ -32,27 +32,26 @@ void init_ioapic(MADT *madt_table){
         map_phys_to_virt_addr(VPTR(io_apic_base_address), (void *) io_apic_hh_base_address, 0);
         _bitmap_set_bit(ADDRESS_TO_BITMAP_ENTRY(io_apic_base_address));
         uint32_t ioapic_version = read_io_apic_register(IO_APIC_VER_OFFSET);
-        loglinef(Verbose, "IOAPIC Version: 0x%x", ioapic_version);
+        loglinef(Info, "(init_ioapic): IOAPIC Version: 0x%x", ioapic_version);
         io_apic_redirect_entry_t redtbl_entry;
         int return_value = read_io_apic_redirect(0x10, &redtbl_entry);
         if (return_value != 0) {
-            logline(Verbose, "PANIC!!");
+            logline(Fatal, "(init_ioapic):  PANIC!!");
         }
         io_apic_max_redirections = (uint8_t) (ioapic_version >> 16);
-        loglinef(Verbose, "Max redirection entries value: 0x%x ", io_apic_max_redirections);
+        loglinef(Verbose, "(init_ioapic): Max redirection entries value: 0x%x ", io_apic_max_redirections);
         io_apic_source_override_array_size = parse_io_apic_interrupt_source_overrides(madt_table);
-        loglinef(Verbose, "Found %x source override entries", io_apic_source_override_array_size);
-        loglinef(Verbose, "---- SO Item: bus_source: %x - irq_source: %x - GSI: %x", io_apic_source_overrides[1].bus_source, io_apic_source_overrides[1].irq_source, io_apic_source_overrides[1].global_system_interrupt);
+        loglinef(Verbose, "(init_ioapic):  Found %x source override entries", io_apic_source_override_array_size);
     }
 }
 
 int parse_io_apic_interrupt_source_overrides(MADT* table) {
-    loglinef(Verbose, "madt_address: 0x%x", (uint64_t) table);
+    loglinef(Verbose, "(parse_io_apic_interrupt_source_overrides): 0x%x", (uint64_t) table);
     uint32_t total_length = sizeof(MADT);
     uint32_t source_override_counter = 0;
     //MADT_Item* item = (MADT_Item *) ((uint32_t)table + sizeof(MADT));
     MADT_Item* item = get_MADT_item(table, MADT_IO_APIC_INTERRUPT_SOURCE_OVERRIDE, source_override_counter);
-    loglinef(Verbose, "Table Lenght: %x", table->header.Length);
+    loglinef(Verbose, "(parse_io_apic_interrupt_source_overrides): Table Lenght: %x", table->header.Length);
     while(total_length < table->header.Length && source_override_counter < IO_APIC_SOURCE_OVERRIDE_MAX_ITEMS) {
         total_length = total_length + item->length;
         if(item != NULL && item->type == MADT_IO_APIC_INTERRUPT_SOURCE_OVERRIDE){
@@ -61,7 +60,7 @@ int parse_io_apic_interrupt_source_overrides(MADT* table) {
             io_apic_source_overrides[source_override_counter].irq_source = so_item->irq_source;
             io_apic_source_overrides[source_override_counter].global_system_interrupt = so_item->global_system_interrupt;
             io_apic_source_overrides[source_override_counter].flags = so_item->flags;
-            loglinef(Verbose, "--SO Item: type: %x  bus_src: %x - irq_src: %x - GSI: %x -", item->type, so_item->bus_source, so_item->irq_source, so_item->global_system_interrupt);
+            loglinef(Verbose, "(parse_io_apic_interrupt_source_overrides): SO Item: type: %x  bus_src: %x - irq_src: %x - GSI: %x -", item->type, so_item->bus_source, so_item->irq_source, so_item->global_system_interrupt);
             source_override_counter++;
         }
         //item = (MADT_Item *)((uint32_t)table + total_length);
@@ -103,7 +102,7 @@ int write_io_apic_redirect(uint8_t index, io_apic_redirect_entry_t redtbl_entry)
     if ((index%2) != 0) {
         return -1;
     }
-    loglinef(Verbose, "Setting redirect entry: %x", index);
+    loglinef(Verbose, "(write_io_apic_redirect):  redirect entry: %x", index);
     uint32_t upper_part = (uint32_t) (redtbl_entry.raw >> 32);
     uint32_t lower_part = (uint32_t) redtbl_entry.raw;
     /* loglinef(Verbose, ": Lower part: %x, : upper part: %x", lower_part, upper_part); */
@@ -129,7 +128,7 @@ void set_irq(uint8_t irq_type, uint8_t redirect_table_pos, uint8_t idt_entry, ui
         // 2. If yes we need to use the gsi in the source override entry
         if(io_apic_source_overrides[counter].irq_source == irq_type) {
             selected_pin = io_apic_source_overrides[counter].global_system_interrupt;
-            loglinef(Verbose, "---Source Override found for type: %x using apic pin: %x ", irq_type, selected_pin);
+            loglinef(Verbose, "(set_irq): ---Source Override found for type: %x using apic pin: %x ", irq_type, selected_pin);
             if((io_apic_source_overrides[counter].flags & 0b11) == 2) {
                 entry.pin_polarity = 0b1;
             } else  {
@@ -147,11 +146,11 @@ void set_irq(uint8_t irq_type, uint8_t redirect_table_pos, uint8_t idt_entry, ui
     // Set the destination api
     entry.destination_field = destination_field;
     entry.interrupt_mask = masked;
-    loglinef(Verbose, "Setting IRQ number: %x, to idt_entry: %x at REDTBL pos: %x - Final value: %x", irq_type, idt_entry, redirect_table_pos, entry.raw);
+    loglinef(Info, "(set_irq): Setting IRQ number: %x, to idt_entry: %x at REDTBL pos: %x - Final value: %x", irq_type, idt_entry, redirect_table_pos, entry.raw);
     write_io_apic_redirect(redirect_table_pos, entry);
     io_apic_redirect_entry_t read_entry; 
     int ret_val = read_io_apic_redirect(IOREDTBL1, &read_entry);
-    loglinef(Verbose, "ret_val: %d - entry raw: %x mask: %d", ret_val, read_entry.vector, read_entry.interrupt_mask);
+    loglinef(Verbose, "(set_irq): ret_val: %d - entry raw: %x mask: %d", ret_val, read_entry.vector, read_entry.interrupt_mask);
 }
 
 int set_irq_mask(uint8_t redirect_table_pos, bool masked_status) {
@@ -162,7 +161,7 @@ int set_irq_mask(uint8_t redirect_table_pos, bool masked_status) {
         read_entry.interrupt_mask = masked_status;
         write_io_apic_redirect(redirect_table_pos, read_entry);
     } else  {
-        logline(Verbose, "Invalid redirection table position");
+        logline(Verbose, "(set_irq_maski): Invalid redirection table position");
         return -1;
     }
     return 0;
