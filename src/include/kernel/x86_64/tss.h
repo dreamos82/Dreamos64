0fd942f29ad5c9077dadbf8efe8468bf6eec6478
Ivan Gualandri <dreamos82@yahoo.it>
2023-10-17 23:58:50 +0100

    Fix kernel_tss struct and load_task_register function

--- src/include/kernel/x86_64/tss.h
+++ src/include/kernel/x86_64/tss.h
@@ -17,6 +17,7 @@ struct tss
     uint64_t rsp1;
     uint64_t rsp2;
     uint64_t reserved1;
+    uint64_t reserved2;
     uint64_t ist1;
     uint64_t ist2;
     uint64_t ist3;
@@ -24,8 +25,8 @@ struct tss
     uint64_t ist5;
     uint64_t ist6;
     uint64_t ist7;
-    uint64_t reserved2;
-    uint16_t reserved3;
+    uint64_t reserved3;
+    uint16_t reserved4;
     uint16_t io_bitmap_offset;
 } __attribute__((packed));
 
--- src/kernel/arch/x86_64/cpu/load_tr.s
+++ src/kernel/arch/x86_64/cpu/load_tr.s
@@ -4,3 +4,4 @@ global _load_task_register
 _load_task_register:
     mov rax, 0x28
     ltr ax
+    ret
