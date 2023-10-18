809f5ee9f1ef27387578c4f13c0d64238f677b2e
Ivan Gualandri <dreamos82@yahoo.it>
2023-10-18 23:40:10 +0100

    Add some comments

--- src/kernel/arch/x86_64/cpu/load_tr.s
+++ src/kernel/arch/x86_64/cpu/load_tr.s
@@ -1,5 +1,7 @@
 section .text
 
+; This function just  load the tss selecto in the task _load_task_register
+; it should be called only once during kernel initialization.
 global _load_task_register
 _load_task_register:
     mov rax, 0x28
