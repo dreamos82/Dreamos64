c1a88e37d5cf4397525e9e43887460aed6721ed5
Ivan Gualandri <dreamos82@yahoo.it>
2023-10-21 17:47:02 +0100

    Change requested

--- src/kernel/arch/x86_64/cpu/tss.c
+++ src/kernel/arch/x86_64/cpu/tss.c
@@ -39,27 +39,36 @@ void initialize_tss(){
 
 void load_tss() {
     // Fields explanation (each entry is 64bit)
-    // TSS_ENTRY_LOW:
-    // 0:15 -> Limit (first 15 bits) should be 0xFFFF
-    // 16:39 -> First 24 bits of kernel_tss address
-    // 40:47 -> Type 4 bits in our case is 1001, 0,  DPL should be 0 , P = 1
-    // 48:55 -> Limit (last 4 bits) can be 0, AVL=available to OS we leave it as 0, 53:54 are 0, 55 G (Granularity)
-    // 55:63 -> Bits 25:31 of the kernel_tss base address
-    // TSS_ENTRY_HIGH
-    // 0:31 -> kernel_tss bits 32:63
-    // 32:63 -> Reserved / 0
     // TYPE: 1001 (64Bit TSS Available)
     // BASE_ADDRESS: kernel_tss
     // LIMIT 16:19 0 DPL: 0 P: 1 G:0
+
     gdt64[TSS_ENTRY_LOW] = 0x00;
     gdt64[TSS_ENTRY_HIGH] = 0x00;
-    gdt64[TSS_ENTRY_LOW] = ((((uint64_t) &kernel_tss>>24) & 0xFF)<<56)  /*bits 56:63*/ | ((uint64_t) (0 & 0xFF) << 48) /* bits 48:55 */ | ((uint64_t)0x89 << 40) /* bits 40:47*/|  ((((uint64_t) &kernel_tss) & 0xFFFFFF) << 16) /* bits 15:39 */ | (uint64_t) 0x70 /* Base */;
-    gdt64[TSS_ENTRY_HIGH] = (uint64_t) 0 |  (((uint64_t) &kernel_tss>>32)& 0xFFFFFFFF);
+
+    // TSS_ENTRY_LOW:
+    uint16_t limit_low = (uint16_t) sizeof(kernel_tss); // 0:15 -> Limit (first 15 bits) should be 0xFFFF
+    uint16_t tss_entry_base_1 = (((uint64_t)&kernel_tss & 0xFFFF));     // 16:31 -> First 16 bits of kernel_tss address
+    uint8_t tss_entry_base_2 = (((uint64_t)&kernel_tss >> 16) & 0xFF); // 32:39 -> Next 8 bits of kernel_tss address
+    uint8_t  flags_1 = 0x89; // 40:47 -> Type 4 bits in our case is 1001, 0,  DPL should be 0 , P = 1
+    uint8_t flags_2 = 0; // 48:55 -> Limit (last 4 bits) can be 0, AVL=available to OS we leave it as 0, 53:54 are 0, 55 G (Granularity)
+    uint8_t tss_entry_base_3 = (((uint64_t)&kernel_tss >> 24) & 0xFF);     // 55:63 -> Bits 25:31 of the kernel_tss base address
+    // TSS_ENTRY_HIGH
+    uint32_t tss_entry_base_4 = (((uint64_t) &kernel_tss>>32)& 0xFFFFFFFF); // 0:31 -> kernel_tss bits 32:63
+    uint32_t reserved_part = 0; // 32:63 -> Reserved / 0
+
+    uint64_t entry_low = (uint64_t) tss_entry_base_3 << 56 | (uint64_t) flags_2 << 48 | (uint64_t) flags_1 << 40 | (uint64_t) tss_entry_base_2 << 32| (uint64_t)tss_entry_base_1 << 16 | (uint64_t) limit_low;
+    uint64_t entry_high = reserved_part | tss_entry_base_4;
+
+
+    gdt64[TSS_ENTRY_LOW] = entry_low;
+    gdt64[TSS_ENTRY_HIGH] = entry_high;
     loglinef(Verbose, "(%s) Loading TSS Register", __FUNCTION__);
     loglinef(Verbose, "(%s) kernel_tss address = 0x%x", __FUNCTION__, &kernel_tss);
+    loglinef(Verbose, "(%s) gdt64[4]n = 0x%x", __FUNCTION__, entry_low);
+    loglinef(Verbose, "(%s) gdt64[5]n = 0x%x", __FUNCTION__,  entry_high);
     loglinef(Verbose, "(%s) gdt64[4] = 0x%x", __FUNCTION__, (uint64_t)gdt64[TSS_ENTRY_LOW]);
     loglinef(Verbose, "(%s) gdt64[5] = 0x%x", __FUNCTION__,  (uint64_t)gdt64[TSS_ENTRY_HIGH]);
 
     _load_task_register();
-    //asm("ltr $0x28");
 }
