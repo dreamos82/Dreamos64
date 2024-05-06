44b212a751144d4c552855a02e1283375a98a5eb
Ivan Gualandri <dreamos82@yahoo.it>
2024-05-07 00:28:35 +0100

    Update tests

--- tests/test_vm.c
+++ tests/test_vm.c
@@ -7,6 +7,7 @@
 #include <vmm_util.h>
 
 
+void test_ensure_address_in_higher_half();
 void test_is_address_higher_half();
 void test_vm_parse_flags();
 
@@ -25,6 +26,20 @@ int main() {
 }
 
 void test_ensure_address_in_higher_half() {
+    printf("Testing ensure_addres_in_higher_half\n");
+    uint64_t test_address = 0x100000;
+    printf("\t [test_vm](%s): Should return (0) for type not recognized and address: 0x%x\n", __FUNCTION__,  test_address);
+    test_address = ensure_address_in_higher_half(test_address, 3);
+    assert(test_address == 0);
+    test_address = ensure_address_in_higher_half(0x100000, VM_TYPE_MMIO);
+    printf("\t [test_vm](%s): Should return (0x%x) for type MMIO and address=0x%x \n", __FUNCTION__,  test_address, 0x100000);
+    assert(test_address == 0xffff800000300000);
+    test_address = ensure_address_in_higher_half(0x100000, VM_TYPE_MEMORY);
+    printf("\t [test_vm](%s): Should return (0x%lx) for type MEMORY and address=0x%x\n", __FUNCTION__,  test_address, 0x100000);
+    assert(test_address == 0xffff800280300000);
+    test_address = ensure_address_in_higher_half(0xffff800280300000, VM_TYPE_MEMORY);
+    printf("\t [test_vm](%s): Should return (0x%lx) for type MEMORY and address=0x%lx\n", __FUNCTION__,  test_address, 0xffff800280300000);
+    assert(test_address == 0xffff800280300000);
 
 }
 
@@ -39,7 +54,7 @@ void test_is_address_higher_half() {
     printf("\t [test_vm](is_address_higher_half): Should return false (0) for 0xffff100000 - %d\n", is_hh);
     assert(is_hh == false);
     is_hh = is_address_higher_half(0xFFFF800000000000);
-    printf("\t [test_vm](is_address_higher_half): Should return false (0) 0xFFFF800000000000 -  %d\n", is_hh);
+    printf("\t [test_vm](is_address_higher_half): Should return true (1) 0xFFFF800000000000 -  %d\n", is_hh);
     assert(is_hh == true);
 }
 
