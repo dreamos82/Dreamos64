42961b0d8cc71950bf15e0d7497206f8e194836a
Ivan Gualandri <dreamos82@yahoo.it>
2025-04-21 12:11:57 +0100

    minor updates

--- src/drivers/ps2/ps2_keyboard_driver.c
+++ src/drivers/ps2/ps2_keyboard_driver.c
@@ -1,5 +1,5 @@
-#include <logging.h>
 #include <devices.h>
+#include <logging.h>
 #include <ps2_keyboard_driver.h>
 
 device_t _ps2_default_driver;
@@ -8,5 +8,10 @@ void _ps2_keyboard_driver_init() {
 
     pretty_log(Info, "Registering ps2 keboard driver");
     _ps2_default_driver.device_id = _device_descriptor++;
+    _ps2_default_driver.drivers = NULL;
+}
 
+
+void _ps2_keyboard_read() {
+    // TODO: this function will be the read function for the keyboard
 }
